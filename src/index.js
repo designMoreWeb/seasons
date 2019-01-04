350a96e621a81fd3460bcb0d0da243fd5365ab28
designMoreWeb <divystolia@gmail.com>
2019-01-04 16:55:19 -0500

    geolocation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App =()=>{
+    window.navigator.geolocation.getCurrentPosition(
+        position=> console.log(position),
+        err => console.log(err)
+    );
     return <div>Hi</div>
 };
-
-
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
