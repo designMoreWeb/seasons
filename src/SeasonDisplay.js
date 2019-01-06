caaa4532cf18c609daafd23800405fda30cbb14f
designMoreWeb <divystolia@gmail.com>
2019-01-06 17:54:03 -0500

    passing seasondisplay state as props

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 
-const SeasonDisplay = () => {
+const SeasonDisplay = (props) => {
+
 return <div>Season Displays</div>
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
-    
+
     state={lat:null,errorMessage:''};
 
     componentDidMount() {
@@ -17,7 +18,7 @@ render(){
     }
 
     if(!this.state.errorMessage && this.state.lat){
-        return <div>Latitude: {this.state.lat}</div>
+        return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>
