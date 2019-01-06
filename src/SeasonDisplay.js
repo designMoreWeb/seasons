7952499cd4ff4dffe5bc7b48544d91608cb9f0ee
designMoreWeb <divystolia@gmail.com>
2019-01-06 18:15:11 -0500

    icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -9,9 +9,9 @@ const getSeason =(lat,month)=>{
 }
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat,new Date().getMonth());
-
-    console.log(season);
-return <div>Season Display</div>
+    const text = season === 'Winter' ? 'Knits & Jackets! hoodie season has hit us' : 'Time for surfing and beach';
+    const icon = season === 'Winter' ? 'snowflake' : 'sun';
+return <div><i className={`${icon}icon`} /><h1>{text}</h1><i className ={`${icon} icon`}/></div>
 };
 
 export default SeasonDisplay;
