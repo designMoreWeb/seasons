c5ca6782c1cc6415f0fea12745fb4112de7d1664
designMoreWeb <divystolia@gmail.com>
2019-01-06 18:19:46 -0500

    clean up code

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const seasonConfig={
+    Summer:{
+        text: 'Knits & Jackets! hoodie season has hit us',
+        iconName: 'sun'
+    },
+    Winter:{
+        text: 'Time for surfing and beach',
+        iconName:'snowflake'
+    }
+};
+
+
 const getSeason =(lat,month)=>{
     if(month > 2 && month < 9){
         return lat > 0 ? 'Summer' : 'Winter';
@@ -9,9 +21,8 @@ const getSeason =(lat,month)=>{
 }
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat,new Date().getMonth());
-    const text = season === 'Winter' ? 'Knits & Jackets! hoodie season has hit us' : 'Time for surfing and beach';
-    const icon = season === 'Winter' ? 'snowflake' : 'sun';
-return <div><i className={`${icon}icon`} /><h1>{text}</h1><i className ={`${icon} icon`}/></div>
+    const {text, iconName} =seasonConfig[season];
+return <div><i className={`${iconName}icon`} /><h1>{text}</h1><i className ={`${iconName} icon`}/></div>
 };
 
 export default SeasonDisplay;
