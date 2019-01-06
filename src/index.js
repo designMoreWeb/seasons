86960aa28fb532ae9d44f809697fe251ad479a04
designMoreWeb <divystolia@gmail.com>
2019-01-06 15:51:13 -0500

    comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
+    //Specific to JS not react
+    constructor(){
+
+    }
+    //Need render method 
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
