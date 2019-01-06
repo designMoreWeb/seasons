178ece513f4821334a7d2e2d3a53d14aec3e8401
designMoreWeb <divystolia@gmail.com>
2019-01-06 16:40:22 -0500

    error hadling

--- src/index.js
+++ src/index.js
@@ -2,24 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
     //Specific to JS not react
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state={lat: null,long:null};
+        this.state = {
+            lat: null,
+            long: null,
+            errorMessage: ''
+        };
     }
     //Need render method 
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => {
-                this.setState({lat: position.coords.latitude});
-                this.setState({long: position.coords.longitude})
+                this.setState({
+                    lat: position.coords.latitude
+                });
+                this.setState({
+                    long: position.coords.longitude
+                })
             },
-            err => console.log(err)
+            err => {
+                this.setState({
+                    errorMessage: err.message
+                });
+            }
         );
-        return <div> Latitude:{this.state.lat}, Longtitude: {this.state.long}</div>
+        return <div > Latitude: {
+                this.state.lat
+            }, Longtitude: {
+                this.state.long
+            }
+            <br />
+            Error:{this.state.errorMessage}
+            </div>
     }
 }
 
-ReactDOM.render(
-    <App />, document.querySelector('#root')
+ReactDOM.render( <
+    App / > , document.querySelector('#root')
 );
