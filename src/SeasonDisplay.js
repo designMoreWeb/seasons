fe4ee38cf7cb29c9a44d3e5cafc146c892480c26
designMoreWeb <divystolia@gmail.com>
2019-01-07 12:01:00 -0500

    styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,12 +1,13 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig={
     Summer:{
-        text: 'Knits & Jackets! hoodie season has hit us',
+        text: 'Time for surfing and beach',
         iconName: 'sun'
     },
     Winter:{
-        text: 'Time for surfing and beach',
+        text: 'Knits & Jackets! hoodie season has hit us's,
         iconName:'snowflake'
     }
 };
@@ -22,7 +23,13 @@ const getSeason =(lat,month)=>{
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat,new Date().getMonth());
     const {text, iconName} =seasonConfig[season];
-return <div><i className={`${iconName}icon`} /><h1>{text}</h1><i className ={`${iconName} icon`}/></div>
+return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className ={`icon-right massive ${iconName} icon`}/>
+        </div>
+        )
 };
 
 export default SeasonDisplay;
